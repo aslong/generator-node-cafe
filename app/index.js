331ff61454e2ac77d02bd13c6c9818b708772acd
Andrew Long <aslong87@gmail.com>
2014-06-14 21:30:49 -0700

    Initial commit of skeleton of generator from generate function

---
+++ app/index.js
@@ -0,0 +1,62 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+var chalk = require('chalk');
+
+
+var CoffeeNodeGenerator = yeoman.generators.Base.extend({
+  method1: function () {
+    console.log("method 1 just ran")
+  },
+
+  method2: function () {
+    console.log("method 2 just ran")
+  },
+
+  init: function () {
+    this.pkg = require('../package.json');
+
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.installDependencies();
+      }
+    });
+  },
+
+  askFor: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay('Welcome to the marvelous CoffeeNode generator!'));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'someOption',
+      message: 'Would you like to enable this option?',
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.someOption = props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  app: function () {
+    this.mkdir('app');
+    this.mkdir('app/templates');
+
+    this.copy('_package.json', 'package.json');
+    this.copy('_bower.json', 'bower.json');
+  },
+
+  projectfiles: function () {
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('jshintrc', '.jshintrc');
+  }
+});
+
+module.exports = CoffeeNodeGenerator;
